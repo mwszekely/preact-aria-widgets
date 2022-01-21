d6768dcbd53a21f64de017d654c688d5a52d645e
mwszekely <19625841+mwszekely@users.noreply.github.com>
2022-01-21 12:18:48 -0500

    Tooltips should close when escape is pressed

--- src/use-tooltip.ts
+++ src/use-tooltip.ts
@@ -1,5 +1,5 @@
 import { h } from "preact";
-import { MergedProps, useHasFocus, UseHasFocusPropsReturnType, useMergedProps, usePassiveState, useRandomId, UseRandomIdPropsReturnType, useRefElement, UseReferencedIdPropsReturnType, useStableCallback, useState, useTimeout } from "preact-prop-helpers";
+import { MergedProps, useGlobalHandler, useHasFocus, UseHasFocusPropsReturnType, useMergedProps, usePassiveState, useRandomId, UseRandomIdPropsReturnType, useRefElement, UseReferencedIdPropsReturnType, useStableCallback, useState, useTimeout } from "preact-prop-helpers";
 import { useCallback, useEffect } from "preact/hooks";
 
 export type UseTooltipTrigger = <TriggerType extends Element>() => { useTooltipTriggerProps: <P extends h.JSX.HTMLAttributes<TriggerType>>({ ...props }: P) => UseReferencedIdPropsReturnType<MergedProps<TriggerType, { onPointerEnter: (e: MouseEvent) => void; onPointerLeave: (e: MouseEvent) => void; }, h.JSX.HTMLAttributes<TriggerType>>, "aria-describedby">; }
@@ -12,6 +12,24 @@ export function useAriaTooltip({ mouseoverDelay, mouseoutDelay, focusDelay }: { 
     mouseoverDelay ??= 400;
     mouseoutDelay ??= 40;
     focusDelay ??= 1;
+
+    // The escape key should close tooltips, but do nothing else.
+    // (i.e. closing a tooltip in a dialog MUST NOT close the dialog too)
+    // TODO: Tooltips are, effectively, always the topmost component,
+    // so we can just have them listen to and swallow all "Escape"
+    // key presses before anyone else. For a more general popup,
+    // or a tooltip in a tooltip (!!) a different solution would be needed.
+    useGlobalHandler(document, "keydown", (e: KeyboardEvent) => {
+        if (getOpen() && e.key === "Escape" && !e.defaultPrevented) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            setOpen(false);
+            setTriggerHoverDelayCorrected(false);
+            setTooltipHoverDelayCorrected(false);
+            setTriggerFocusedDelayCorrected(false);
+            setTooltipFocusedDelayCorrected(false);
+        }
+    }, { capture: true });
 
     const [open, setOpen, getOpen] = useState(false);
 
