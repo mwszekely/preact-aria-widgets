a9d836967d7886aa1a1e41dac04c0c331003392d
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-02-07 00:33:13 -0500

    Update dependencies

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -3,7 +3,7 @@ import { useImperativeHandle } from "preact/hooks";
 import { useButton, UseButtonParameters, UseButtonReturnType } from "../use-button";
 import { memoForwardRef, PartialExcept } from "./util";
 
-type Get<T, K extends keyof T> = T[K];
+type Get<T, K extends keyof T> = NonNullable<T[K]>;
 
 interface ButtonPropsBase<E extends Element> extends
     Omit<Get<UseButtonParameters<E>, "buttonParameters">, "role">,
